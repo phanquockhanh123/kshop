1126e3280a02bb6240e549ca0767dd79e786f6d5
phanquockhanh123 <khanhphanquoc68@gmail.com>
2024-03-15 23:49:52 +0700

    handle api intergraition with javascript

--- app/Services/FabbiService.php
+++ app/Services/FabbiService.php
@@ -10,14 +10,49 @@ class FabbiService implements FabbiServiceInterface
 {
     public function saveJson(array $data)
     {
+        $resultSave = [];
+        $saveData = [];
+        $id = 1;
+
+        foreach ($data['dishes'] as $dt) {
+            $jsonDishes = json_decode($dt['dishes'], true);
+
+            foreach ($jsonDishes as $dish) {
+                $saveData[] = [
+                    'id' => $id,
+                    'name' => $dish,
+                    'restaurant' => $dt['restaurant'],
+                    'availableMeals' => [$dt['meal']]
+                ];
+            }
+            $id++;
+        }
+
+        // Now, let's group dishes by name and merge available meals
+        $groupedData = [];
+        foreach ($saveData as $item) {
+            $name = $item['name'];
+            if (!isset($groupedData[$name])) {
+                $groupedData[$name] = $item;
+            } else {
+                $groupedData[$name]['availableMeals'] = array_merge($groupedData[$name]['availableMeals'], [$item['availableMeals'][0]]);
+            }
+        }
+
+        // Re-index the grouped data
+        $groupedData = array_values($groupedData);
+
         try {
-            dd(12);
-            if (file_exists(storage_path('./storage/app/dishes.json'))) {
+            $filePath = 'dishes.json';
+            if (Storage::exists($filePath)) {
                 $json = File::get(storage_path('app/dishes.json'));
                 $oldData = json_decode($json, true);
-                $data = [$oldData, $data];
+                $resultSave['dishes'] = array_merge($oldData['dishes'] ?? [], $groupedData);
+            } else {
+                $resultSave['dishes'] = $groupedData;
             }
-            Storage::disk('local')->put('dishes.json', json_encode($data));
+
+            Storage::disk('local')->put('dishes.json', json_encode($resultSave));
         } catch (\Throwable $th) {
             \Log::error($th);
 
